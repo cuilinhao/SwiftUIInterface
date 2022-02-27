502563e72d46d06a2319e72f660866f654c56cd9
Linhao CUI 崔林豪 <linhao.cui@nio.com>
2022-02-27 16:28:03 +0800

    add view

--- SwiftUI_Demo/SwiftUIViewDemo.swift
+++ SwiftUI_Demo/SwiftUIViewDemo.swift
@@ -12,10 +12,7 @@ struct SwiftUIViewDemo: View {
     var plans : [String] = ["1", "2","3","4","5","6","1", "2","3","4","5","6","1", "2","3","4","5","6","1", "2","3","4","5","6","1", "2","3","4","5","6","1", "2","3","4","5","6",]
     
     @State var isActiveTest : Bool = true
-    
-    
     var body: some View {
-        
         ScrollView {
             
             LazyVStack(alignment: .leading){
@@ -23,16 +20,55 @@ struct SwiftUIViewDemo: View {
                     
                     switch index {
                     case 0 :
-                        HStack {
+                        NavigationLink(
+                            destination: AnimationDemo(),  label: {
+                                Image("111")
+                                    .resizable()
+                                    .frame(width: 50, height: 50)
+                                    .cornerRadius(25)
+                                Text("GeometryReaderView")
+                                    .foregroundColor(Color.purple)
+                                    .padding()
+                            })
+                    case 1:
+                        HStack(spacing: 20) {
                             Image("111")
                                 .resizable()
                                 .frame(width: 50, height: 50)
                                 .cornerRadius(25)
-                            Text("AnimationDemo")
-                                .foregroundColor(Color.pink)
+                            Button("this is a button") {
+                                print("test")
+                            }
+                            .padding()
+                        }
+                        .background(Color.green)
+                        //.frame(width: UIScreen.main.bounds.size.width, height: 30)
+                    case 2:
+                        self.configurePushVc()
+                    case 3:
+                        NavigationLink(destination: VehicleSwiftUIView(fontSize: 30, foregroundColor: .yellow, backgroundColor: .blue, content: {
+                            
+                            Button("test Vehicle"){
+                                print("test Vehicle")
+                            }
+                        })) {
+                            Image("111")
+                                .resizable()
+                                .frame(width: 50, height: 50)
+                                .cornerRadius(25)
+                            Text("VehicleSwiftUIView")
+                                .foregroundColor(.purple)
                                 .padding()
                         }
-                       
+                    case 4:
+                        //let ss = TableViewDemo()
+                        self.__configurePushVc {
+                            return TableViewDemo()
+                        }
+                    case 5:
+                        self.___configurePushVc({
+                            return StackDemo()
+                        }, title: "StackDemo")
                     default:
                         HStack {
                             Image("111")
@@ -48,11 +84,61 @@ struct SwiftUIViewDemo: View {
             }
         }
     }
-    
-    
       
 }
 
+extension SwiftUIViewDemo {
+    
+    //一个view的实例作为一个参数进行传递
+    func configurePushVc() -> some View {
+        VStack {
+            NavigationLink(
+                destination: AnimationDemo(),  label: {
+                    Image("111")
+                        .resizable()
+                        .frame(width: 50, height: 50)
+                        .cornerRadius(25)
+                    Text("GeometryReaderView")
+                        .foregroundColor(Color.purple)
+                        .padding()
+                })
+        }
+    }
+    
+    //一个view的实例作为一个参数进行传递
+    func __configurePushVc<Content: View>(_ vv: () -> Content) -> some View {
+        VStack {
+            NavigationLink(
+                destination: vv,  label: {
+                    Image("111")
+                        .resizable()
+                        .frame(width: 50, height: 50)
+                        .cornerRadius(25)
+                    Text("TableViewDemo")
+                        .foregroundColor(Color.purple)
+                        .padding()
+                })
+        }
+    }
+    
+    
+    //一个view的实例作为一个参数进行传递
+    func ___configurePushVc<Content: View>(_ vv: () -> Content, title: String) -> some View {
+        VStack {
+            NavigationLink(
+                destination: vv,  label: {
+                    Image("111")
+                        .resizable()
+                        .frame(width: 50, height: 50)
+                        .cornerRadius(25)
+                    Text(title)
+                        .foregroundColor(Color.purple)
+                        .padding()
+                })
+        }
+    }
+    
+}
 
 @ViewBuilder func cellView() -> some View {
     
